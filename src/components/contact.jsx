6dcda8d1fc99b640230d5a8762f6a95c7998e222
Viacheslav Pliusnin <via.plsn@gmail.com>
2021-05-18 11:37:23 +0300

    Contact.jsx code optimization

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,60 +3,31 @@ import {ReactComponent as Male} from '../assets/images/man-icon.svg';
 import {ReactComponent as Female} from '../assets/images/woman-icon.svg';
 
 const Contact = ({firstName, lastName, phone, gender}) => {
-    if(gender === "male") {
-        return(
-            <div className="contact-container">
-                <div className="firstName">
-                    <p>{firstName}</p>
-                </div>
-                <div className="lastName">
-                    <p>{lastName}</p>
-                </div>
-                <div className="phone">
-                    <p>{phone}</p>
-                </div>
-                <div className="gender">
-                    <p>{gender}</p>
-                    <Male/>
-                </div>
-            </div>
-        )
-    } else if(gender === "female") {
-        return(
-            <div className="contact-container">
-                <div className="firstName">
-                    <p>{firstName}</p>
-                </div>
-                <div className="lastName">
-                    <p>{lastName}</p>
-                </div>
-                <div className="phone">
-                    <p>{phone}</p>
-                </div>
-                <div className="gender">
-                    <p>{gender}</p>
-                    <Female/>
-                </div>
-            </div>
-        )
-    } else {
-        return(
-            <div className="contact-container">
-                <div className="firstName">
-                    <p>{firstName}</p>
-                </div>
-                <div className="lastName">
-                    <p>{lastName}</p>
-                </div>
-                <div className="phone">
-                    <p>{phone}</p>
-                </div>
-                <div className="gender">
-                    <p>{gender}</p>
-                </div>
-            </div>
-        )
+    const renderIcon = (gender) => {
+        if(gender === 'male') {
+        return <Male />
+        } else if (gender === 'female') {
+        return <Female />
+        }
+        return null
     }
+    return(
+        <div className="contact-container">
+            <div className="firstName">
+                <p>{firstName}</p>
+            </div>
+            <div className="lastName">
+                <p>{lastName}</p>
+            </div>
+            <div className="phone">
+                <p>{phone}</p>
+            </div>
+            <div className="gender">
+                <p>{gender}</p>
+                {renderIcon(gender)}
+            </div>
+        </div>
+    )
 }
 
 export default Contact;
